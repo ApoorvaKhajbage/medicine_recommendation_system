6697b73da4d7deaf56ffadc05da70bcf8e58b417
Murtuza <murtuzakapasi5253@gmail.com>
2024-03-28 15:47:31 +0530

    ML model completed with preprocessing

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, render_template
+from sklearn.decomposition import TruncatedSVD
 import numpy as np
 import pandas as pd
 import pickle
+
 
 app = Flask(__name__)
 
@@ -18,6 +20,7 @@ diets = pd.read_csv("datasets/diets.csv")
 #===================================== load model===========================================
 
 svc = pickle.load(open('models/svc_model.pkl','rb'))
+svd = pickle.load(open('models/svd_model.pkl','rb'))
 
 #==========================================================================================
 # custom helping functions
@@ -59,12 +62,35 @@ diseases_list = {15: 'Fungal infection', 4: 'Allergy', 16: 'GERD', 9: 'Chronic c
                  3: 'Alcoholic hepatitis', 36: 'Tuberculosis', 10: 'Common Cold', 34: 'Pneumonia', 13: 'Dimorphic hemmorhoids(piles)', 18: 'Heart attack', 39: 'Varicose veins', 26: 'Hypothyroidism', 24: 'Hyperthyroidism', 25: 'Hypoglycemia',
                  31: 'Osteoarthristis', 5: 'Arthritis', 0: '(vertigo) Paroymsal  Positional Vertigo', 2: 'Acne', 38: 'Urinary tract infection', 35: 'Psoriasis', 27: 'Impetigo'}
 
-# Model Prediction function
-def get_predicted_value(patient_symptoms):
+
+
+
+# Define preprocess_input function
+# Define preprocess_input function
+def preprocess_input(patient_symptoms):
+    # Convert patient symptoms to input vector
     input_vector = np.zeros(len(symptoms_dict))
     for item in patient_symptoms:
         input_vector[symptoms_dict[item]] = 1
-    return diseases_list[svc.predict([input_vector])[0]]
+    
+    # Apply TruncatedSVD transformation
+    input_vector_transformed = svd.transform([input_vector])
+    
+    return input_vector_transformed
+
+# Define get_predicted_value function
+def get_predicted_value(patient_symptoms):
+    # Preprocess input
+    input_vector_transformed = preprocess_input(patient_symptoms)
+    
+    # Make prediction
+    predicted_disease_index = svc.predict(input_vector_transformed)[0]
+    
+    # Map index to actual disease name
+    predicted_disease = diseases_list[predicted_disease_index]
+    
+    return predicted_disease
+
 
 #=========================================== creating routes========================================
 @app.route('/')
